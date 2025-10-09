f0cb935e8653fd48d891270574f5644c77f8407b
Voopieq <domantas.kelmanas2003@gmail.com>
2025-10-09 17:15:07 +0300

    UI text fixes
    -Code should cover all edge cases for UI changes

--- Assets/Scripts/Shop/ShoppingCart.cs
+++ Assets/Scripts/Shop/ShoppingCart.cs
@@ -13,9 +13,12 @@ namespace Shop
         private List<Item> cartItems;
         private int _totalPrice;
 
+        private List<Item> cartItemsBought; // Used to check if the cart has been emptied after purchase.
+
         private void Start()
         {
             cartItems = new List<Item>();
+            cartItemsBought = new List<Item>();
             
             // Reset the text to default
             checkoutDisplayText.fontSize = 0.05f;
@@ -27,13 +30,12 @@ namespace Shop
         {
             // Is object an interactable item?
             Item item = other.GetComponentInParent<Item>();
-            if (item != null)
+            if (item != null && cartItemsBought.Count == 0)
             {
-                ItemData itemData = item.GetItemData();
-                
                 // Item hasn't been bought before. Allow to buy.
                 if (!item.IsBought)
                 {
+                    ItemData itemData = item.GetItemData();
                     cartItems.Add(item);
                     _totalPrice += itemData.itemPrice;
                     
@@ -51,10 +53,24 @@ namespace Shop
             Item item = other.GetComponentInParent<Item>();
             if (item != null)
             {
-                ItemData itemData = item.GetItemData();
-
-                if (!item.IsBought)
+                // Item was purchased but not removed. Remove it.
+                if (cartItemsBought.Contains(item))
                 {
+                    cartItemsBought.Remove(item);
+
+                    if (cartItemsBought.Count == 0)
+                    {
+                        checkoutDisplayText.fontSize = 0.05f;
+                        checkoutDisplayText.color = Color.red;
+                        checkoutDisplayText.text = "No Items";
+                    }
+                    
+                    return;
+                }
+                
+                if (!item.IsBought && cartItems.Contains(item))
+                {
+                    ItemData itemData = item.GetItemData();
                     cartItems.Remove(item);
                     _totalPrice -= itemData.itemPrice;
                     
@@ -63,7 +79,7 @@ namespace Shop
                     checkoutDisplayText.color = Color.green;
                     checkoutDisplayText.text = "$" + _totalPrice;
 
-                    if (_totalPrice <= 0)
+                    if (cartItems.Count == 0)
                     {
                         checkoutDisplayText.fontSize = 0.05f;
                         checkoutDisplayText.color = Color.red;
@@ -79,23 +95,28 @@ namespace Shop
             
             if (ShopManager.Instance.TryPurchase(cartItems))
             {
+                // Add cart items to a new list (to keep track when all items had been taken from cart after purchase)
+                foreach (Item item in cartItems)
+                {
+                    cartItemsBought.Add(item);
+                }
                 cartItems.Clear();
+
+                _totalPrice = 0;
                 // TODO: Put these lines in a method for reusability
                 checkoutDisplayText.fontSize = 0.08f;
                 checkoutDisplayText.color = Color.green;
                 checkoutDisplayText.text = "$$$";
                 
                 await Task.Delay(2000);
-                
-                checkoutDisplayText.fontSize = 0.04f;
-                checkoutDisplayText.color = Color.green;
-                checkoutDisplayText.text = "Take Items";
-            }
-        }
 
-        public void ClearShoppingCart()
-        {
-            cartItems.Clear();
+                if (cartItemsBought.Count != 0)
+                {
+                    checkoutDisplayText.fontSize = 0.04f;
+                    checkoutDisplayText.color = Color.green;
+                    checkoutDisplayText.text = "Take Items";
+                }
+            }
         }
     }
 }
