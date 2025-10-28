2d751b10d5cb1701ecc1676f88ec794214fa9f05
B-Rakevicius <115837262+B-Rakevicius@users.noreply.github.com>
2025-10-28 19:50:55 +0200

    Revert "Merge branch 'main' of https://github.com/B-Rakevicius/VrRepo"
    This reverts commit e016a31e9d7daf267ddbdcdd582eae7ac07499a2, reversing
    changes made to 8312b675bb104839ec9b583978bbdb89ea759faa.

--- Assets/Scripts/Items/IWeapon.cs
+++ Assets/Scripts/Items/IWeapon.cs
@@ -6,11 +6,5 @@ namespace Items
         /// Start using the weapon.
         /// </summary>
         public void UseWeapon();
-
-        /// <summary>
-        /// Reload current weapon.
-        /// </summary>
-        /// <returns>True, if possible to reload. False otherwise.</returns>
-        public bool TryReload();
     }
 }
