254e6133b4092c58ae32addad2386969aebbb28d
Voopieq <domantas.kelmanas2003@gmail.com>
2025-10-28 18:37:26 +0200

    Crossbow physical reloading and shooting
    -Ability to pick up arrows and load them to crossbow. One arrow loads 5 arrows internaly

--- Assets/Scripts/Items/IWeapon.cs
+++ Assets/Scripts/Items/IWeapon.cs
@@ -6,5 +6,11 @@ namespace Items
         /// Start using the weapon.
         /// </summary>
         public void UseWeapon();
+
+        /// <summary>
+        /// Reload current weapon.
+        /// </summary>
+        /// <returns>True, if possible to reload. False otherwise.</returns>
+        public bool TryReload();
     }
 }
